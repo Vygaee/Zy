3cecb4b23103cbed54fa35fc1d6f333d17e21ed0
Vooyage <148488760+Vygaee@users.noreply.github.com>
2024-01-22 16:55:06 +0700

    Update bot.py

--- Musikku/core/bot.py
+++ Musikku/core/bot.py
@@ -1,52 +1,50 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-""""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
+#
+# Copyright (C) 2021-2022 by kenkansaja@Github, < https://github.com/kenkansaja >.
+#
+# This file is part of < https://github.com/kenkansaja/Musikku > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/kenkansaja/Musikku/blob/master/LICENSE >
+#
+# All rights reserved.
 
 import sys
 
 from pyrogram import Client
+
 import config
+
 from ..logging import LOGGER
-from pyrogram.enums import ChatMemberStatus
 
 
 class MusikkuBot(Client):
     def __init__(self):
+        LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
-            "MusicBot",
+            "Musikku",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
-            in_memory=True,
         )
-        LOGGER(__name__).info(f"Starting Bot...")
 
     async def start(self):
         await super().start()
         get_me = await self.get_me()
         self.username = get_me.username
         self.id = get_me.id
-        self.mention = get_me.mention
         try:
             await self.send_message(
-                config.LOG_GROUP_ID, "» ᴍᴜsɪᴄ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ, ᴡᴀɪᴛɪɴɢ ғᴏʀ ᴀssɪsᴛᴀɴᴛ..."
+                config.LOG_GROUP_ID, "Bot Started"
             )
         except:
             LOGGER(__name__).error(
-                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
+                "Bot gagal mengakses Grup log. Pastikan Anda telah menambahkan bot ke saluran log dan dipromosikan sebagai admin!"
             )
             sys.exit()
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
-        if a.status != ChatMemberStatus.ADMINISTRATOR:
-            LOGGER(__name__).error("Please promote Bot as Admin in Logger Group")
+        if a.status != "administrator":
+            LOGGER(__name__).error(
+                "Tolong promosikan Bot sebagai Admin di Grup Logger"
+            )
             sys.exit()
         if get_me.last_name:
             self.name = get_me.first_name + " " + get_me.last_name
